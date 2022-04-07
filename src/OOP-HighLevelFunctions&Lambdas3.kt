67adc60f1ae0705baef4f71f9a2c7caaff42c173
Prathamesh Kumbhar <prathameshkumbhar291113@gmail.com>
2022-04-07 16:40:47 +0530

    "Committed By Prathamesh"

--- src/OOP-HighLevelFunctions&Lambdas3.kt
+++ src/OOP-HighLevelFunctions&Lambdas3.kt
@@ -1,3 +1,18 @@
 //Closures in Kotlin
 //In java 8 u cannot change(Mutate) the values of outside variables inside lambda
-//Hiiiiiiiiii yssssssss everyone
+//But in kotlin you can change the values
+//Soo closures technically means that we can store the  values of the lambda inside the outside variables
+
+fun main(args: Array<String>) {
+    var mlp = MyLambdaClosures()
+    var result = 0
+    val myLambda : (Int,Int) ->Unit = {x,y-> result = x+y}
+    mlp.addTwoNumbers(76,1){x,y-> result = x+y}
+    println("The additional value of the $result")
+}
+class MyLambdaClosures{
+    fun addTwoNumbers(a: Int , b:Int,myFunc:(Int,Int) ->Unit){
+        myFunc(a,b)
+    }
+}
+
